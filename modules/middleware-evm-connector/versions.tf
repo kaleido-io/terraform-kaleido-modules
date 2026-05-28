dfcc8511936f94d5c50bda629a1ad5c4af4b0a84
hfuss <hayden.fuss@kaleido.io>
2026-05-28 00:34:41 -0400

    lint fr
    Signed-off-by: hfuss <hayden.fuss@kaleido.io>

--- modules/middleware-evm-connector/versions.tf
+++ modules/middleware-evm-connector/versions.tf
@@ -2,10 +2,8 @@ terraform {
   required_version = ">= 1.13.0"
   required_providers {
     kaleido = {
-    kaleido = {
       source = "kaleido-io/kaleido"
       version = "~> 1.3.0"
-    }
     }
   }
 }
