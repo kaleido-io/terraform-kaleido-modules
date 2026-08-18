de7e1c67e21a8c4a8dff1876b68a824cd1a6b82c
John Hosie <john.hosie@kaleido.io>
2026-08-18 22:52:43 +0100

    output connector flow ids
    Signed-off-by: John Hosie <john.hosie@kaleido.io>

--- modules/middleware-evm-connector/outputs.tf
+++ modules/middleware-evm-connector/outputs.tf
@@ -18,14 +18,37 @@ output "submission_flow_name" {
   description = "Name of the deployed submission connector flow."
 }
 
+output "submission_flow_id" {
+  value       = kaleido_platform_connector_flow.submission.id
+  description = "ID of the deployed submission connector flow. Needed to bind a workflow subflow to it - subflow bindings take an ID, not a name."
+}
+
 output "query_flow_name" {
   value       = kaleido_platform_connector_flow.query.name
   description = "Name of the deployed query connector flow."
 }
 
+output "query_flow_id" {
+  value       = kaleido_platform_connector_flow.query.id
+  description = "ID of the deployed query connector flow. Needed to bind a workflow subflow to it - subflow bindings take an ID, not a name."
+}
+
+output "flow_ids" {
+  value = {
+    submission = kaleido_platform_connector_flow.submission.id
+    query      = kaleido_platform_connector_flow.query.id
+  }
+  description = "Map of connector flow name to deployed flow ID."
+}
+
 output "standard_api_name" {
   value       = kaleido_platform_connector_standard_api.evm.name
   description = "Name of the deployed EVM standard API."
+}
+
+output "standard_api_id" {
+  value       = kaleido_platform_connector_standard_api.evm.id
+  description = "ID of the deployed EVM standard API."
 }
 
 output "stream_factories" {
